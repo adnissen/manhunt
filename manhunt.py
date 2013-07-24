c9de20631460685f4c909de7996368d60bdd359a
Andrew Nissen <andrew_nissen@yahoo.com>
2013-07-24 11:24:17 -0700

    added a bunch of comments

--- manhunt.py
+++ manhunt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# written by Andrew Nissen
+
 import sys
 import requests
 import xml.etree.ElementTree as ET
@@ -7,6 +9,7 @@ import yaml
 import urllib
 
 def loadConfig():
+	# load all the config vars we need in global vars
 	global config
 	global sabHost
 	global sabPort
@@ -17,6 +20,7 @@ def loadConfig():
 	sabapiKey = config["sabapiKey"]
 
 def sabConfig():
+	# quick and dirty wizard to configure sabnzbd+
 	entry = raw_input("Enter your sabnzbd+ host (example: localhost): ")
 	config["sabHost"] = entry
 	entry = raw_input("Enter your sabnzbd+ port (example: 8080): ")
@@ -28,19 +32,15 @@ def sabConfig():
 	yaml.dump(config, stream)
 
 def addIndex():
+	# another quick wizard to make adding indexers easier
 	indexers = yaml.load(open('indexers.yaml'))
 	name = raw_input("Name of indexer (example: nzbs.org): ")
-	url = raw_input("Url of indexer (example: http://nzbs.org/): ")
+	url = raw_input("Url of indexer (example: http://nzbs.org/ [don't forget the / !]): ")
 	apikey = raw_input("Api Key (example: 60fa49af916bc402916eeec6440757e7): ")
 	indexers[str(name)] = [url, apikey]
 
 	stream = file('indexers.yaml', 'w')
 	yaml.dump(indexers, stream)
-
-def writeApiKey(key):
-	stream = file('config.yaml', 'w')
-	config["apikey"] = key
-	yaml.dump(config, stream)
 
 def search(searchTerm):
 	global resUrls
@@ -50,7 +50,8 @@ def search(searchTerm):
 	global total
 	total = 1
 
-	indexers = yaml.load(open('indexers.yaml'))
+	indexers = yaml.load(open('indexers.yaml')) 
+	# for every indexer we have, make a request
 	for indexer in indexers:
 		request = requests.get(indexers[indexer][0] + 'api?lang=EN&t=search&q='+ searchTerm + '&apikey=' + indexers[indexer][1])
 		if (r.status_code == 200):
@@ -67,6 +68,7 @@ def search(searchTerm):
 				total += 1
 		else:
 			print("Request failed, check to make sure indexers.yaml is correct.")
+	# a tiny bit of error handling so it doesn't silently exit (crashes are fine though)
 	if (total > 1):
 		getInput()
 	else:
@@ -74,6 +76,7 @@ def search(searchTerm):
 		return
 
 def getInput():
+	# display the entries, prompt for which one they want to download
 	entry = raw_input('Entry to download: ')
 	if (entry == 'q'):
 		return
@@ -84,12 +87,14 @@ def getInput():
 	addToSab(resUrls[int(entry) - 1], resNames[int(entry) - 1])
 
 def addToSab(url, name):
+	# make request to sab
 	url = urllib.quote(url, '')
 	sabUrl = "http://" + str(sabHost) + ":" + str(sabPort) + "/api?mode=addurl&name=" + str(url) + "&nzbname=" + str(name) + "&apikey=" + str(sabapiKey)
 	request = requests.get(sabUrl)
 	if (r.status_code == 404):
 		print("sabnzbd+ not found, check to make sure config.yaml is correct")
 
+# this should probably be in a "__main__", but it doesn't matter since importing it would be dumb to bring with
 loadConfig()
 if (len(sys.argv) == 2 and sys.argv[1] == "--config"):
 	sabConfig()
@@ -98,4 +103,4 @@ elif (len(sys.argv) == 2 and sys.argv[1] == "--addIndex"):
 elif (len(sys.argv) >= 2):
 	search(" ".join(sys.argv[1:]))
 else:
-	print("useage: manhunt \"searchterm\"")
+	print("usage: manhunt \"searchterm\" or \"--config\" or \"--addIndex\"")
